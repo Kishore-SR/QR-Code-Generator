e02f9994228c0f328a238d9ff396d07f98287b89
Kishore S R <kishoresr01@gmail.com>
2024-09-15 18:52:35 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let qrText = document.querySelector('.qr-text');
+let qrCode = document.querySelector('.qrImg');
+let downBtn = document.querySelector('.down-qr');
+
+// Initially download button is disabled
+downBtn.disabled = true;
+
+
+
+
